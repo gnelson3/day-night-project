78348294c2fa1a30e9fe0c8ffa68d26be6d61995
gnelson3 <dnelsoncarter@aol.com>
2016-03-03 01:25:11 -0500

    start day-night project

---
+++ day-night.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+    $('#orb' ).click( function(){
+        if($(this).hasClass('sun')) { 
+           $(this).removeClass('sun').addClass('moon');
+        }
+        else {
+            $(this).removeClass('moon').addClass('sun');
+        }
+        
+        if($('#sky').hasClass('day')) { 
+           $('#sky').removeClass('day').addClass('night');
+            console.log('vxgbdgdtgd')
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+    });
+});
